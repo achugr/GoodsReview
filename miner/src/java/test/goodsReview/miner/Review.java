082628770d838fa131bcf842ece4b04ef6574ec2
AChugr <artemij.chugreev@gmail.com>
2011-10-16 17:03:26 +0400

    class for review

---
+++ miner/src/java/test/goodsReview/miner/Review.java
@@ -0,0 +1,25 @@
+package test.goodsReview.miner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Artemij
+ * Date: 16.10.11
+ * Time: 17:01
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Review {
+    private String comment;
+
+    protected Review(String comment) {
+        this.comment = comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
+
