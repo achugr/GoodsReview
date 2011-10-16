321f3491abbcbb43a9e3cacba71595243c4ccb79
olegpan <oleg.panjkov@yandex.ru>
2011-10-16 23:05:58 +0400

    Simple example of Yalet

--- frontend/src/java/test/GoodsReview/frontend/yalets/SimpleRequestYalet.java
+++ frontend/src/java/test/GoodsReview/frontend/yalets/SimpleRequestYalet.java
@@ -6,6 +6,7 @@ import net.sf.xfresh.core.Yalet;
  * @author OlegPan
  */
 public class SimpleRequestYalet implements Yalet{
+
     public void process(InternalRequest req, InternalResponse res)
     {
         String str = req.getParameter("id");
