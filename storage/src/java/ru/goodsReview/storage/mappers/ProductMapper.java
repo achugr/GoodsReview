85c072d0f7f2d95b9b07b0ac6fbb3ce66073a7ca
AChugr <artemij.chugreev@gmail.com>
2011-10-19 00:26:39 +0400

    model для товара

---
+++ storage/src/java/ru/goodsReview/storage/mappers/ProductMapper.java
@@ -0,0 +1,11 @@
+package ru.goodsReview.storage.mappers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Artemij
+ * Date: 18.10.11
+ * Time: 23:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProductMapper {
+}
---
+++ storage/src/java/ru/goodsReview/storage/model/Product.java
@@ -0,0 +1,11 @@
+package ru.goodsReview.storage.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Artemij
+ * Date: 18.10.11
+ * Time: 23:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class Product {
+}
