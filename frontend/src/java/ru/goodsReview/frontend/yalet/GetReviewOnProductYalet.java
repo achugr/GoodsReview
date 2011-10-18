48d66a802a154a39217bdd9d5e83380bc1dc6a0d
AChugr <artemij.chugreev@gmail.com>
2011-10-18 04:15:57 +0400

    сервер запускается, а дальше как-то все странно

---
+++ frontend/src/java/ru/goodsReview/frontend/yalet/GetReviewOnProductYalet.java
@@ -0,0 +1,23 @@
+package ru.goodsReview.frontend.yalet;
+
+import net.sf.xfresh.core.InternalRequest;
+import net.sf.xfresh.core.InternalResponse;
+import net.sf.xfresh.core.Yalet;
+import net.sf.xfresh.core.xml.Xmler;
+
+/**
+ * @author OlegPan
+ */
+public class GetReviewOnProductYalet implements Yalet {
+
+    public void process(InternalRequest req, InternalResponse res)
+    {
+        String request = req.getParameter("request");
+
+        if (request.isEmpty()) {
+                  Xmler.Tag ans = Xmler.tag("response", "hello, world!");
+                  res.add(ans);
+                  return;
+        }
+    }
+}
---
+++ frontend/src/java/test/goodsReview/frontend/FrontendModuleTest.java
@@ -0,0 +1,33 @@
+package test.goodsReview.frontend;
+
+import net.sf.xfresh.core.ErrorInfo;
+import net.sf.xfresh.core.InternalRequest;
+import net.sf.xfresh.core.InternalResponse;
+import net.sf.xfresh.core.SaxWriter;
+import net.sf.xfresh.jetty.JettyServerInitializer;
+import org.apache.log4j.Logger;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import ru.goodsReview.frontend.yalet.GetReviewOnProductYalet;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Artemij
+ * Date: 18.10.11
+ * Time: 3:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class FrontendModuleTest {
+    private static final Logger log = Logger.getLogger(JettyServerInitializer.class);
+
+    public static void main(String [] args){
+
+        log.info("Server started: " + (System.currentTimeMillis() ) + " ms");
+
+    }
+
+}
