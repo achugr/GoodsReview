03c309b5e6ce875777c5e9a082fd6b864cf50159
AChugr <artemij.chugreev@gmail.com>
2011-10-18 04:17:27 +0400

    на порте 8080 висит jetty, а как вот запрос ялету отдать не знаю

--- frontend/src/java/test/goodsReview/frontend/FrontendModuleTest.java
+++ frontend/src/java/test/goodsReview/frontend/FrontendModuleTest.java
@@ -25,7 +25,177 @@ public class FrontendModuleTest {
     private static final Logger log = Logger.getLogger(JettyServerInitializer.class);
 
     public static void main(String [] args){
+        InternalRequest req=new InternalRequest() {
+            public String getRealPath() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
 
+            public boolean needTransform() {
+                return false;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getParameter(String s) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String[] getParameters(String s) {
+                return new String[0];  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void putAttribute(String s, Object o) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public Object getAttribute(String s) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public Map<String, String> getCookies() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public Map<String, List<String>> getAllParameters() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getRequestURL() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getRequestRoot() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getQueryString() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public int getIntParameter(String s) {
+                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public int getIntParameter(String s, int i) {
+                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public long getLongParameter(String s) {
+                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public long getLongParameter(String s, long l) {
+                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getRemoteAddr() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getHeader(String s) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getCookie(String s) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public Long getUserId() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        };
+        InternalResponse resp = new InternalResponse() {
+            public void redirectTo(String s) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void add(Object o) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public <K, V> void addMap(Map<K, V> kvMap, SaxWriter<Map.Entry<K, V>> entrySaxWriter) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void addWrapped(String s, Object o) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public List<Object> getData() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getRedir() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void addError(ErrorInfo errorInfo) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public List<ErrorInfo> getErrors() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void addCookie(String s, String s1) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void addCookie(String s, String s1, int i) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void addCookie(String s, String s1, int i, String s2) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void addCookie(String s, String s1, int i, String s2, String s3) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void addCookie(String s, String s1, int i, String s2, String s3, boolean b) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void setCookies(Map<String, String> stringStringMap) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void removeCookie(String s) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void clear() {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void setHeader(String s, String s1) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void setHttpStatus(int i) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void setContentType(String s) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public String getContentType() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void setProcessed(boolean b) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public boolean isProcessed() {
+                return false;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public OutputStream getOutputStream() throws IOException {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        };
+        final FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
         log.info("Server started: " + (System.currentTimeMillis() ) + " ms");
 
     }
