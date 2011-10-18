1ca19786215ceb3f52f7db13e4de2517a57e8216
AChugr <artemij.chugreev@gmail.com>
2011-10-18 23:07:15 +0400

    классы для обработки простого запроса

---
+++ frontend/src/java/ru/goodsReview/frontend/controller/GetProductInfo.java
@@ -0,0 +1,11 @@
+package ru.goodsReview.frontend.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Artemij
+ * Date: 18.10.11
+ * Time: 23:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class GetProductInfo {
+}
---
+++ frontend/src/java/ru/goodsReview/frontend/model/BaseRequest.java
@@ -0,0 +1,11 @@
+package ru.goodsReview.frontend.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Artemij
+ * Date: 18.10.11
+ * Time: 23:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseRequest {
+}
