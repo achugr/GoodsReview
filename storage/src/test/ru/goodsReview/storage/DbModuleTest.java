556fc4ec911754cd225212705553057126b5c5e0
AChugr <artemij.chugreev@gmail.com>
2011-10-26 00:31:56 +0400

    вынес класс для теста в отдельную директорию

---
+++ storage/src/test/ru/goodsReview/storage/DbModuleTest.java
@@ -0,0 +1,12 @@
+/*
+    Date: 26.10.11
+    Time: 00:09
+    Author: 
+        Artemij Chugreev 
+        artemij.chugreev@gmail.com
+*/
+
+package ru.goodsReview.storage;
+
+public class DbModuleTest {
+}
