03d3eb334961247ffcd571b179c068d81ef2c68d
AChugr <artemij.chugreev@gmail.com>
2011-10-26 00:38:49 +0400

    директория для теста контроллера

--- storage/src/test/ru/goodsReview/storage/DbModuleTest.java
+++ storage/src/test/ru/goodsReview/storage/DbModuleTest.java
@@ -8,5 +8,26 @@
 
 package ru.goodsReview.storage;
 
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import ru.goodsReview.core.model.Product;
+import ru.goodsReview.storage.controller.ProductDbController;
+
+import javax.activation.DataSource;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DbModuleTest {
+    public static void main(String[] args) throws SQLException {
+            final FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("storage/src/scripts/beans.xml");
+            DataSource dataSource = null;
+            dataSource = (DataSource) context.getBean("dataSource");
+            ProductDbController baseController = new  ProductDbController(new SimpleJdbcTemplate(dataSource));
+            List<Product> productsList = new ArrayList<Product>();
+            productsList.add(new Product(1, "iphone_4", "mobile_phone_by_apple", 10));
+            productsList.add(new Product(1, "htc_desire", "mobile phone by HTC", 6));
+            productsList.add(new Product(1, "nokia_3310", "cool mobile phone by Nokia", 100));
+            baseController.setProductList(productsList);
+        }
 }
