68eef19da6348381481f7f4dadb60345e92552f5
YaroslavSkudarnov <skudarnovyi@gmail.com>
2011-10-26 04:15:00 +0400

    testSearcher, yeah. Use it on your own risk!

--- searcher/src/java/test/goodsReview/searcher/testSearcher.java
+++ searcher/src/java/test/goodsReview/searcher/testSearcher.java
@@ -1,19 +1,19 @@
 package goodsReview.searcher;
 
 import org.apache.lucene.analysis.ru.RussianAnalyzer;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.Version;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
-import ru.goodsReview.core.model.Review;
 import ru.goodsReview.indexer.Indexer;
 
 import javax.sql.DataSource;
 import java.io.File;
-import java.util.ArrayList;
 
 public class testSearcher {
     static final String DBDirectory = "database";
@@ -22,11 +22,26 @@ public class testSearcher {
         DataSource dataSource = null;
         dataSource = (DataSource) context.getBean("dataSource");
         Indexer indexer = new Indexer(new SimpleJdbcTemplate(dataSource));
-        indexer.doIndex(DBDirectory);
+        indexer.doProductsIndex(DBDirectory);
         IndexSearcher indexSearcher = new IndexSearcher(new SimpleFSDirectory(new File(DBDirectory)));
         QueryParser queryParser = new QueryParser(Version.LUCENE_34, "",new RussianAnalyzer(Version.LUCENE_34));
-        Query query = queryParser.parse("name:iPhone 4");
-        ArrayList<Review> reviews = new ArrayList<Review>();
-        //Not written yet. Please, careful!
+
+        Query query = queryParser.parse("name:iPhone 4"); //"good" example
+        TopDocs topDocs = indexSearcher.search(query,1);
+        if (topDocs.getMaxScore() > 0) {
+            Document document = indexSearcher.doc(topDocs.scoreDocs[0].doc); //H-H-H-H-H-H-HOLY SHIT!!!!1
+            System.out.println(document.get("id")+" "+document.get("name")+" "+document.get("popularity")+" "+document.get("description"));
+        } else {
+            System.out.println("Sorry, no results!");
+        }
+
+        Query query1 = queryParser.parse("name:iMNotiPhone5, lol"); //"bad" example
+        TopDocs topDocs1 = indexSearcher.search(query1,1); //Imma freakin' copypaster, yeah!
+        if (topDocs1.getMaxScore() > 0) {
+            Document document = indexSearcher.doc(topDocs1.scoreDocs[0].doc);
+            System.out.println(document.get("id")+" "+document.get("name")+" "+document.get("popularity")+" "+document.get("description"));
+        } else {
+            System.out.println("Sorry, no results!");
+        }
     }
 }
