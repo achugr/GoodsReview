c24bee89dd61b5cfde38ed1c3b5fcaedd5fa601a
Evgeny Vanslov <vans239@gmail.com>
2011-10-30 15:22:04 +0400

    Added sample data script sql. Changed gitignore

---
+++ storage/src/sql/sample.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (name, description, parent_category_id) VALUES("All Products","Everything",1);
+INSERT INTO category (name, description, parent_category_id) VALUES("Electronics","Gadgets",1);
+INSERT INTO category (name, description, parent_category_id) VALUES("Notebooks","",2);
+
+INSERT INTO product (name, popularity, category_id) VALUES("Asus eee pc", 1, 3);
+INSERT INTO product (name, popularity, category_id) VALUES("Lenovo x220", 1, 3);
+INSERT INTO product (name, popularity, category_id) VALUES("Asus u30", 1, 3);
+INSERT INTO product (name, popularity, category_id) VALUES("Apple macbook air", 1, 3);
+INSERT INTO product (name, popularity, category_id) VALUES("Sony vaio z", 1, 3);
+
