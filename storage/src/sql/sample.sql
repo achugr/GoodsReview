184692ad1bed8b86d2fb231b69fb15392da7adfd
Evgeny Vanslov <vans239@gmail.com>
2011-10-31 18:39:05 +0400

    Changed sample.sql aqain))

--- storage/src/sql/sample.sql
+++ storage/src/sql/sample.sql
@@ -9,8 +9,12 @@ INSERT INTO product (name, popularity, category_id) VALUES("Apple macbook air", 
 INSERT INTO product (name, popularity, category_id) VALUES("Sony vaio z", 1, 3);
 
 INSERT INTO source (name, main_page_url) VALUES("Vans Home","brain.ru");
-INSERT INTO review (product_id, content, author, date, description, source_id, source_url, positivity, importance, votes_yes, votes_no) VALUES(1,"Not bad notebook. It is very cheap, but have small batery","I am",NULL,NULL,1,NULL,NULL,NULL,NULL,NULL)
+
+INSERT INTO review (product_id, content, author, date, description, source_id, source_url, positivity, importance, votes_yes, votes_no) VALUES(1,"Not bad notebook. It is very cheap, but have small batery","I am",'2011-10-01',NULL,1,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO review (product_id, content, author, date, description, source_id, source_url, positivity, importance, votes_yes, votes_no) VALUES(1,"SPAM","I am",'2011-10-01', NULL,1,NULL,NULL,NULL,NULL,NULL);
+
 INSERT INTO thesis (review_id, content, positivity, importance, votes_yes, votes_no) VALUES(1,"Not bad notebook",10,10,10,0);
 INSERT INTO thesis (review_id, content, positivity, importance, votes_yes, votes_no) VALUES(1,"It is very cheap",10,10,10,0);
 INSERT INTO thesis (review_id, content, positivity, importance, votes_yes, votes_no) VALUES(1,"but have small batery",10,10,10,0);
+
 
