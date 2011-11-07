74d0f04d680862902fd99dd451a3907e7fa18579
Amarchuk <aamarchuk@gmail.com>
2011-11-07 20:01:45 +0400

    configs changed

--- extractor/src/java/ru/goodsReview/extractor/listener/CitilinkNotebooksScraperRuntimeListener.java
+++ extractor/src/java/ru/goodsReview/extractor/listener/CitilinkNotebooksScraperRuntimeListener.java
@@ -13,22 +13,18 @@ import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.webharvest.runtime.Scraper;
 import org.webharvest.runtime.ScraperRuntimeListener;
 import org.webharvest.runtime.processors.BaseProcessor;
+import ru.goodsReview.core.model.Review;
 import ru.goodsReview.storage.controller.ProductDbController;
 import ru.goodsReview.storage.controller.ReviewDbController;
 
-import java.sql.Timestamp;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
-
-
-//import ru.brandanalyst.core.model.Article;
-//import ru.brandanalyst.miner.util.DataTransformator;
 
 
 public class CitilinkNotebooksScraperRuntimeListener implements ScraperRuntimeListener {
 
     private int i = 0;
-   // private static final Logger log = Logger.getLogger(CitilinkNotebooksScraperRuntimeListener.class);
+    private static final Logger log = Logger.getLogger(CitilinkNotebooksScraperRuntimeListener.class);
 
     protected SimpleJdbcTemplate jdbcTemplate;
     protected ReviewDbController reviewDbController;
@@ -39,22 +35,6 @@ public class CitilinkNotebooksScraperRuntimeListener implements ScraperRuntimeLi
         reviewDbController = new ReviewDbController(jdbcTemplate);
     }
 
-    private Timestamp evalTimestamp(String stringDate) {
-        stringDate = stringDate.replace("\n", "");
-        stringDate = stringDate.replace(" ", "");
-
-        int minute = Integer.parseInt(stringDate.substring(13, 15));
-        int hour = Integer.parseInt(stringDate.substring(10, 12));
-        int day = Integer.parseInt(stringDate.substring(0, 2));
-        int month = Integer.parseInt(stringDate.substring(3, 5));
-        int year = Integer.parseInt(stringDate.substring(6, 10));
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day, hour, minute);
-
-        return new Timestamp(calendar.getTime().getTime());
-    }
-
     public void onExecutionStart(Scraper scraper) {
 
     }
@@ -63,11 +43,9 @@ public class CitilinkNotebooksScraperRuntimeListener implements ScraperRuntimeLi
     }
 
     public void onExecutionContinued(Scraper scraper) {
-
     }
 
     public void onNewProcessorExecution(Scraper scraper, BaseProcessor baseProcessor) {
-
     }
 
     public void onExecutionEnd(Scraper scraper) {
@@ -77,18 +55,10 @@ public class CitilinkNotebooksScraperRuntimeListener implements ScraperRuntimeLi
 
     public void onProcessorExecutionFinished(Scraper scraper, BaseProcessor baseProcessor, Map map) {
 
-        if ("body".equalsIgnoreCase(baseProcessor.getElementDef().getShortElementName()) && (scraper.getRunningLevel() == 5)) {
-            String nameProd =  scraper.getContext().get("ProductName").toString();
-            String ProdPrice = scraper.getContext().get("Price").toString();
-            System.out.println(nameProd + ": " + ProdPrice + "::: "+ scraper.getRunningLevel());
-        }
-
-         /* if ("body".equalsIgnoreCase(scraper.getRunningProcessor().getElementDef().getShortElementName()) && (scraper.getRunningLevel() == 8)) {
+          if ("body".equalsIgnoreCase(scraper.getRunningProcessor().getElementDef().getShortElementName()) && (scraper.getRunningLevel() == 6)) {
 
             String nameProd =  scraper.getContext().get("ProductName").toString();
             String ProdPrice = scraper.getContext().get("Price").toString();
-           // System.out.println(nameProd + ": " + ProdPrice);
-
             String ReviewTime = scraper.getContext().get("ReviewTime").toString();
             String StarRate = scraper.getContext().get("StarRate").toString();
             String Description = scraper.getContext().get("Description").toString();
@@ -97,32 +67,19 @@ public class CitilinkNotebooksScraperRuntimeListener implements ScraperRuntimeLi
             String Comments = scraper.getContext().get("Comments").toString();
             String VoteYes = scraper.getContext().get("VoteYes").toString();
             String VoteNo = scraper.getContext().get("VoteNo").toString();
-            //Review rev =  new Review(1,1, GoodFeatures+BadFeatures+ Comments, "amarch", 10, Description, 15 , "httpa", Integer.parseInt(StarRate), 100500, Integer.parseInt(VoteYes), Integer.parseInt(VoteNo));
-            //reviewDbController.addReview(rev);
-          //  articleProvider.writeArticleToDataStore(article);
-           // log.info(" New review addded");
-           // System.out.println(ReviewTime + " " + StarRate + " ");
-          }   */
+            Date date = new Date(System.currentTimeMillis());
 
-
-            //Variable newsText = (Variable) scraper.getContext().get("newsFullText");
-            //Variable newsDate = (Variable) scraper.getContext().get("newsDate");
-            //long brandId = ((Variable) scraper.getContext().get("brandId")).toLong();
-            //Timestamp articleTimestamp = evalTimestamp(newsDate.toString());
-
-           // String articleContent = DataTransformator.clearString(newsText.toString());
-           // System.out.println(articleContent);
-            //String articleTitle = newsTitle.toString();
-
-            //  Article article = new Article(-1, brandId, 10, articleTitle, articleContent, articleLink, articleTimestamp, 0);
-
-
-
+             //System.out.println(nameProd);
+             Review rev =  new Review(1,1, GoodFeatures+"\n"+BadFeatures+"\n"+Comments, "amarch", date, Description, 15 , "httpa",
+                                    Integer.parseInt(StarRate), 100500, Integer.parseInt(VoteYes), Integer.parseInt(VoteNo));
+            reviewDbController.addReview(rev);
+            log.info(" New review addded");
+          }
     }
 
     public void onExecutionError(Scraper scraper, Exception e) {
         if (e != null) {
-            //log.error("CitilinkNotebooksScraperRuntimeListener error");
+            log.error("CitilinkNotebooksScraperRuntimeListener error");
         }
     }
 }
