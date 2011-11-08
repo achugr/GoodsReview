e8d378943d3f9744dd22826282fe85984c5a028e
AChugr <artemij.chugreev@gmail.com>
2011-11-08 16:24:26 +0400

    strange class :)

--- miner/src/java/ru/goodsReview/miner/GrabberContainer.java
+++ miner/src/java/ru/goodsReview/miner/GrabberContainer.java
@@ -1,11 +1,31 @@
 package ru.goodsReview.miner;
+
 /*
- *  Date: 08.11.11
- *   Time: 15:20
- *   Author: 
- *      Artemij Chugreev 
- *      artemij.chugreev@gmail.com
- */
+    Date: 10/26/11
+    Time: 06:20
+    Author: Alexander Marchuk
+            aamarchuk@gmail.com
+*/
 
 public class GrabberContainer {
+    /*
+    private static final Logger log = Logger.getLogger(GrabberContainer.class);
+
+    private List<Grabber> grabberList;
+
+    public void setGrabberList(List<Grabber> grabberList) {
+        this.grabberList = grabberList;
+    }
+
+    public void afterPropertiesSet() {
+
+        log.info("miner started...");
+        if (grabberList != null) {
+            for (Grabber g : grabberList) {
+                g.grab();
+            }
+        }
+        log.info("miner finished.");
+    }
+    */
 }
