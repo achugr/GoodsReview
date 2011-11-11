3ff0294e6cd8851d6fc865615f43c8d6f7fd35a9
YaroslavSkudarnov <skudarnovyi@gmail.com>
2011-11-12 00:28:43 +0400

    added all needed indexer methods, fixed some build bug

--- indexer/src/java/ru/goodsReview/indexer/Indexer.java
+++ indexer/src/java/ru/goodsReview/indexer/Indexer.java
@@ -9,12 +9,8 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.Version;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
-import ru.goodsReview.core.model.Product;
-import ru.goodsReview.core.model.Review;
-import ru.goodsReview.core.model.Thesis;
-import ru.goodsReview.storage.controller.ProductDbController;
-import ru.goodsReview.storage.controller.ReviewDbController;
-import ru.goodsReview.storage.controller.ThesisDbController;
+import ru.goodsReview.core.model.*;
+import ru.goodsReview.storage.controller.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -63,7 +59,7 @@ public class Indexer extends TimerTask {
             document.add(new Field("name", product.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
             String desc;
             if ((desc = product.getDescription()) != null) {
-                document.add(new Field("description", product.getDescription(), Field.Store.YES, Field.Index.ANALYZED));
+                document.add(new Field("description", desc, Field.Store.YES, Field.Index.ANALYZED));
             } else {
                 document.add(new Field("description", "", Field.Store.YES, Field.Index.ANALYZED));
             }
@@ -82,12 +78,33 @@ public class Indexer extends TimerTask {
             document = new Document();
             document.add(new Field("id",Long.toString(review.getId()),Field.Store.YES,Field.Index.NOT_ANALYZED));
             document.add(new Field("productId",Long.toString(review.getProductId()),Field.Store.YES,Field.Index.NOT_ANALYZED));
-            document.add(new Field("content",review.getContent(),Field.Store.NO, Field.Index.ANALYZED));
+            String content;
+            if ((content = review.getContent()) != null) {
+                document.add(new Field("content",content,Field.Store.NO,Field.Index.ANALYZED));
+            } else {
+                document.add(new Field("content",null,Field.Store.NO,Field.Index.ANALYZED));
+            }
+            String author;
+            if ((author = review.getContent()) != null) {
+                document.add(new Field("author",author,Field.Store.NO,Field.Index.ANALYZED));
+            } else {
+                document.add(new Field("author",null,Field.Store.NO,Field.Index.ANALYZED));
+            }
             document.add(new Field("author",review.getAuthor(),Field.Store.NO,Field.Index.ANALYZED));
             document.add(new Field("date",review.getDate().toString(),Field.Store.NO,Field.Index.ANALYZED));
-            document.add(new Field("description",review.getDescription(),Field.Store.NO,Field.Index.NO));
+            String desc;
+            if ((desc = review.getDescription()) != null) {
+                document.add(new Field("description", desc, Field.Store.NO,Field.Index.NO));
+            } else {
+                document.add(new Field("description", "", Field.Store.NO,Field.Index.NO));
+            }
             document.add(new Field("sourceId",Long.toString(review.getSourceId()),Field.Store.NO,Field.Index.NO));
-            document.add(new Field("sourceUrl",review.getSourceUrl(),Field.Store.NO,Field.Index.NO));
+            String sourceURL;
+            if ((sourceURL = review.getSourceUrl()) != null) {
+                document.add(new Field("sourceUrl", sourceURL, Field.Store.NO, Field.Index.NO));
+            } else {
+                document.add(new Field("sourceUrl", "", Field.Store.NO, Field.Index.NO));
+            }
             document.add(new Field("positivity",Double.toString(review.getPositivity()),Field.Store.NO,Field.Index.NOT_ANALYZED));
             document.add(new Field("importance",Double.toString(review.getImportance()),Field.Store.NO,Field.Index.NOT_ANALYZED));
             document.add(new Field("votesYes",Integer.toString(review.getVotesYes()),Field.Store.NO,Field.Index.NOT_ANALYZED));
@@ -97,7 +114,7 @@ public class Indexer extends TimerTask {
         finish();
     }
 
-    public void doThesisIndex(String directoryDB) throws Exception {
+    public void doThesesIndex(String directoryDB) throws Exception {
         init(directoryDB);
         List<Thesis> theses = new ThesisDbController(jdbcTemplate).getAllTheses();
         Document document;
@@ -106,13 +123,58 @@ public class Indexer extends TimerTask {
             document.add(new Field("id",Long.toString(thesis.getId()),Field.Store.YES,Field.Index.NOT_ANALYZED));
             document.add(new Field("reviewId",Long.toString(thesis.getReviewId()),Field.Store.YES,Field.Index.NOT_ANALYZED));
             document.add(new Field("thesisUniqueId",Long.toString(thesis.getThesisUniqueId()),Field.Store.NO, Field.Index.ANALYZED));
-            document.add(new Field("content",thesis.getContent(),Field.Store.NO, Field.Index.ANALYZED));
+            String content;
+            if ((content = thesis.getContent()) != null) {
+                document.add(new Field("content",content,Field.Store.NO, Field.Index.ANALYZED));
+            } else {
+                document.add(new Field("content",null,Field.Store.NO, Field.Index.ANALYZED));
+            }
             document.add(new Field("frequency",Integer.toString(thesis.getFrequency()),Field.Store.NO,Field.Index.NOT_ANALYZED));
             document.add(new Field("positivity",Double.toString(thesis.getPositivity()),Field.Store.NO,Field.Index.NOT_ANALYZED));
             document.add(new Field("importance",Double.toString(thesis.getImportance()),Field.Store.NO,Field.Index.NOT_ANALYZED));
             writer.addDocument(document);
         }
         finish();
+    }
+
+    public void doThesesUniqueIndex(String directoryDB) throws Exception {
+        init(directoryDB);
+        List<ThesisUnique> thesesUnique = new ThesisUniqueDbController(jdbcTemplate).getAllThesesUnique();
+        Document document;
+        for (ThesisUnique thesisUnique : thesesUnique) {
+            document = new Document();
+            document.add(new Field("id",Long.toString(thesisUnique.getId()),Field.Store.YES,Field.Index.NOT_ANALYZED));
+            String content;
+            if ((content = thesisUnique.getContent()) != null) {
+                document.add(new Field("content",content,Field.Store.NO, Field.Index.ANALYZED));
+            } else {
+                document.add(new Field("content",null,Field.Store.NO, Field.Index.ANALYZED));
+            }
+            document.add(new Field("frequency",Integer.toString(thesisUnique.getFrequency()),Field.Store.NO,Field.Index.NOT_ANALYZED));
+            document.add(new Field("lastScan",thesisUnique.getLastScan().toString(),Field.Store.NO,Field.Index.NOT_ANALYZED));
+            document.add(new Field("positivity",Double.toString(thesisUnique.getPositivity()),Field.Store.NO,Field.Index.NOT_ANALYZED));
+            document.add(new Field("importance",Double.toString(thesisUnique.getImportance()),Field.Store.NO,Field.Index.NOT_ANALYZED));
+            writer.addDocument(document);
+        }
+        finish();
+    }
+
+    public void doCategoriesIndex(String directoryDB) throws Exception {
+        init(directoryDB);
+        List<Category> categories = new CategoryDbController(jdbcTemplate).getAllCategories();
+        Document document;
+        for (Category category : categories) {
+            document = new Document();
+            document.add(new Field("id",Long.toString(category.getId()),Field.Store.NO,Field.Index.NOT_ANALYZED));
+            document.add(new Field("parentCategoryId",Long.toString(category.getParentCategoryId()),Field.Store.NO,Field.Index.NOT_ANALYZED));
+            document.add(new Field("name",category.getName(),Field.Store.YES, Field.Index.ANALYZED));
+            String desc;
+            if ((desc = category.getDescription()) != null) {
+                document.add(new Field("description", desc, Field.Store.NO, Field.Index.ANALYZED));
+            } else {
+                document.add(new Field("description", "", Field.Store.NO, Field.Index.ANALYZED));
+            }
+        }
     }
 
     public void run() {
