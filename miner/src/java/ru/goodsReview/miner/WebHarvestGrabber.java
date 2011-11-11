571e561b6644a45d8bdf64c32c42f89bc1dc4d32
Amarchuk <aamarchuk@gmail.com>
2011-11-11 15:12:17 +0400

    class WebHarvestGrabber added

---
+++ miner/src/java/ru/goodsReview/miner/WebHarvestGrabber.java
@@ -0,0 +1,12 @@
+/*
+ *  Date: 11/11/11
+ *   Time: 15:01
+ *   Author: 
+ *      Artemij Chugreev 
+ *      artemij.chugreev@gmail.com
+ */
+
+package ru.goodsReview.miner;
+
+public class WebHarvestGrabber {
+}
