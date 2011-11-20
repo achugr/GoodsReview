b092889312f59deceb7d01d034441f47f1fb018c
YaroslavSkudarnov <skudarnovyi@gmail.com>
2011-11-20 23:24:56 +0400

    New package wordAnalyzer, new class AdjectiveAnalyzer and main method in it created

---
+++ backend/src/java/ru/goodsReview/backend/wordAnalyzer/AdjectiveAnalyzer.java
@@ -0,0 +1,41 @@
+package ru.goodsReview.backend.wordAnalyzer;
+
+/*
+    Date: 20.11.11
+    Time: 22:46
+    Author: 
+        Yaroslav Skudarnov 
+        SkudarnovYI@gmail.com
+*/
+
+public class AdjectiveAnalyzer {
+
+    /**
+     * Checks if word is an adjective.
+     * @param word Word which is tested for being adjective.
+     * @return True if word is an adjective, false — otherwise.
+     */
+    public static boolean isAdjective (String word) {
+        word = word.trim();
+        if (word.indexOf(" ") != 1) {
+            throw new IllegalArgumentException("Word mustn't have a spaces.");
+        } else if (word.length() < 3) {
+            throw new IllegalArgumentException("Too small word.");
+        } else {
+            String end = word.substring(word.length() - 2, word.length());
+            if ((end.equals("ой")) || (end.equals("го"))
+                    || (end.equals("му")) || (end.equals("им"))
+                    || (end.equals("ом")) || (end.equals("ий"))
+                    || (end.equals("ем")) || (end.equals("ым"))
+                    || (end.equals("ый")) || (end.equals("ей"))
+                    || (end.equals("их")) || (end.equals("яя"))
+                    || (end.equals("ая")) || (end.equals("ее"))
+                    || (end.equals("ья")) || (end.equals("ое"))
+                    || (end.equals("ье")) || (end.equals("ие"))) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
