9b8d76f0f6f25ee0a9e27bf212964cb3343464f0
AChugr <artemij.chugreev@gmail.com>
2011-12-24 11:54:18 +0400

    util for creating thesis extraction document

---
+++ analyzer/src/java/ru/goodsReview/analyzer/util/ThesisExtractionDocument.java
@@ -0,0 +1,11 @@
+package ru.goodsReview.analyzer.util;
+/*
+ *  Date: 24.12.11
+ *   Time: 01:46
+ *   Author: 
+ *      Artemij Chugreev 
+ *      artemij.chugreev@gmail.com
+ */
+
+public class ThesisExtractionDocument {
+}
