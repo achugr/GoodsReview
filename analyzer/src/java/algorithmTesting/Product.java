5ad06ddfa7f27cdb908c053a2ae1bde6ae38deef
IlyaMakeev <makeev.pole-position@yandex.ru>
2012-02-05 22:24:49 +0400

    Update analyzer/src/java/algorithmTesting/Product.java

--- analyzer/src/java/algorithmTesting/Product.java
+++ analyzer/src/java/algorithmTesting/Product.java
@@ -1,11 +1,11 @@
 package algorithmTesting;
 
 /**
- * Created by IntelliJ IDEA.
- * User: IlyaMakeev
  * Date: 05.02.12
- * Time: 18:15
- * To change this template use File | Settings | File Templates.
+ * Time: 21:18
+ * Author:
+ * Ilya Makeev
+ * ilya.makeev@gmail.com
  */
 import java.util.ArrayList;
 
