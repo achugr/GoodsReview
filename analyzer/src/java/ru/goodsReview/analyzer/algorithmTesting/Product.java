3f8aeae0c2c2fef62599dd9985fb4d059f96af53
AChugr <artemij.chugreev@gmail.com>
2012-02-08 21:01:15 +0400

    AlgorithmTesting

---
+++ analyzer/src/java/ru/goodsReview/analyzer/algorithmTesting/Product.java
@@ -0,0 +1,21 @@
+package ru.goodsReview.analyzer.algorithmTesting;
+
+/**
+ * Date: 05.02.12
+ * Time: 21:18
+ * Author:
+ * Ilya Makeev
+ * ilya.makeev@gmail.com
+ */
+import java.util.ArrayList;
+
+public class Product {
+    String name;
+    ArrayList<Review> reviews;
+
+    public Product(String name, ArrayList<Review> reviews) {
+        this.name = name;
+        this.reviews = reviews;
+    }
+
+}
