0b9839d50238cea651a15d4f66fc080974a63954
AChugr <artemij.chugreev@gmail.com>
2012-02-12 23:12:13 +0400

    update

--- analyzer/src/java/ru/goodsReview/analyzer/util/ThesisPattern.java
+++ analyzer/src/java/ru/goodsReview/analyzer/util/ThesisPattern.java
@@ -14,31 +14,59 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * class to represent thesis patterns
+ */
 public class ThesisPattern {
+//    list of part of speech - this is pattern
     List<PartOfSpeech> pattern;
+    MystemAnalyzer mystemAnalyzer;
 
-    public ThesisPattern(List<PartOfSpeech> pattern){
+    /**
+     * create new pattern
+     * @param pattern
+     */
+    public ThesisPattern(List<PartOfSpeech> pattern) throws IOException {
         this.pattern = pattern;
+        mystemAnalyzer = new MystemAnalyzer();
     }
 
-    public ThesisPattern(PartOfSpeech p1, PartOfSpeech p2){
+    /**
+     * create new pattern
+     * @param p1 first token of pattern
+     * @param p2 second token of pattern
+     */
+    public ThesisPattern(PartOfSpeech p1, PartOfSpeech p2) throws IOException {
         pattern = new ArrayList<PartOfSpeech>();
         pattern.add(p1);
         pattern.add(p2);
+        mystemAnalyzer = new MystemAnalyzer();
     }
 
-    public ThesisPattern(PartOfSpeech p1, PartOfSpeech p2, PartOfSpeech p3){
+    /**
+     * create new pattern
+     * @param p1 first token of pattern
+     * @param p2 second token of pattern
+     * @param p3 third token of pattern
+     */
+    public ThesisPattern(PartOfSpeech p1, PartOfSpeech p2, PartOfSpeech p3) throws IOException {
         pattern = new ArrayList<PartOfSpeech>();
         pattern.add(p1);
         pattern.add(p2);
         pattern.add(p3);
+        mystemAnalyzer = new MystemAnalyzer();
     }
 
+    /**
+     * matches with this pattern
+     * @param sourceThesis possibly this is thesis
+     * @return true if sourceThesis matches with pattern, false - otherwise
+     * @throws IOException
+     */
     public boolean match(List<String> sourceThesis) throws IOException {
         if(sourceThesis.size() != pattern.size()){
             return false;
         }
-        MystemAnalyzer mystemAnalyzer = new MystemAnalyzer();
         int i=0;
         for(String sourceThesisToken : sourceThesis){
             if(!mystemAnalyzer.partOfSpeech(sourceThesisToken).equals(pattern.get(i))){
@@ -50,7 +78,12 @@ public class ThesisPattern {
     }
     
     public static void main(String [] args){
-        ThesisPattern thesisPattern = new ThesisPattern(PartOfSpeech.NOUN, PartOfSpeech.ADJECTIVE);
+        ThesisPattern thesisPattern = null;
+        try {
+            thesisPattern = new ThesisPattern(PartOfSpeech.NOUN, PartOfSpeech.ADJECTIVE);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
         {
             List<String> source = new ArrayList<String>();
             source.add("ноут");
