ae364e61a43252babe9b2dd2b986b7c4e7656ebe
ilya makeev <ilya.makeev@gmail.com>
2012-03-07 23:12:21 +0400

    update ThesisExtractionTestDocument

--- analyzer/src/java/ru/goodsReview/analyzer/algorithmTesting/ThesisExtractionTestDocument.java
+++ analyzer/src/java/ru/goodsReview/analyzer/algorithmTesting/ThesisExtractionTestDocument.java
@@ -34,46 +34,70 @@ public class ThesisExtractionTestDocument {
 
         ArrayList<Review> reviewsList = new ArrayList<Review>();
         ArrayList<String> thesisList = new ArrayList<String>();
-        String r = "null";
-        String productId = "null";
+        String reviewID = "-1";
+        String productName = "-1";
         String s = in.readLine();
 
         while (s != null) {
             s = s.trim();
-            s = s.toLowerCase();
 
             if (s.contains("<product id=")) {
-                if (!productId.equals("null")) {
-                    reviewsList.add(new Review(r, (ArrayList<String>) thesisList.clone()));
+                if (!productName.equals("-1")) {
+                    reviewsList.add(new Review(reviewID, (ArrayList<String>) thesisList.clone()));
                     thesisList.clear();
-                    r = "null";
+                    reviewID = "-1";
 
-                    ProductList.add(new Product(productId, (ArrayList<Review>) reviewsList.clone()));
+                    ProductList.add(new Product(productName, (ArrayList<Review>) reviewsList.clone()));
                     reviewsList.clear();
                 }
-                productId = s.substring(s.indexOf("=") + 2, s.indexOf("name") - 2);
+                productName = s.substring(s.indexOf("name=") + 6, s.lastIndexOf("\""));
             }
 
-            if (s.contains("<review")) {
-                if (!r.equals("null")) {
-                    reviewsList.add(new Review(r, (ArrayList<String>) thesisList.clone()));
-                    thesisList.clear();
+            if (!productName.equals("-1")) {
+                if (s.contains("<review")) {
+                    if (!reviewID.equals("-1")) {
+                        reviewsList.add(new Review(reviewID, (ArrayList<String>) thesisList.clone()));
+                        thesisList.clear();
+                    }
+                    reviewID = s.substring(s.indexOf("\"") + 1, s.lastIndexOf("\""));
                 }
-                r = s.substring(s.indexOf("\"") + 1, s.lastIndexOf("\""));
-            }
 
-            if (s.contains("<thesis>")) {
-                String t = s.substring(s.indexOf("<thesis>") + 8, s.indexOf("</thesis>")).trim();
-                thesisList.add(t);
-            }
+                if (s.contains("##")) {
+                    String t = s.substring(0, s.indexOf("##")).trim();
 
-            s = in.readLine();
+                    if (!t.equals("")) {
+                        if (t.contains(",")) {
+                            String[] arr = t.split(",");
+                            for (int i = 0; i < arr.length; i++) {
+                                String s1 = arr[i];
+                                s1 = s1.trim();
+                                s1 = splitBracket(s1);
+                                if (!thesisList.contains(s1)) {
+                                    thesisList.add(s1);
+                                }
+                            }
+                        } else {
+                            thesisList.add(splitBracket(t));
+                        }
+                    }
+
+                }
+
+                s = in.readLine();
+            }
         }
 
-        reviewsList.add(new Review(r, thesisList));
-        ProductList.add(new Product(productId, reviewsList));
+        reviewsList.add(new Review(reviewID, thesisList));
+        ProductList.add(new Product(productName, reviewsList));
 
         return ProductList;
+    }
+
+    static  String splitBracket(String s){
+        if(s.contains("[")){
+            s = s.substring(0, s.indexOf("["));
+        }
+        return  s;
     }
 
     // comparison of thesis for two products lists
@@ -81,7 +105,16 @@ public class ThesisExtractionTestDocument {
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filePath)));
 
         for (int i = 0; i < humProThesis.size(); i++) {
-            comparator(algoProThesis.get(i), humProThesis.get(i), out);
+            Product humProduct = humProThesis.get(i);
+
+            for (int j = 0; j < algoProThesis.size(); j++) {
+                Product algoProduct = algoProThesis.get(j);
+                if(editDist(algoProduct.getName(), humProduct.getName())<2){
+                    comparator(algoProduct, humProduct, out);
+                    break;
+                }
+            }
+
         }
 
         out.flush();
@@ -89,7 +122,7 @@ public class ThesisExtractionTestDocument {
 
     // comparison of thesis for two products
     static void comparator(Product algoProduct, Product humProduct, PrintWriter out) {
-        out.println("<product id=\"" + algoProduct.getName() + "\">");
+        out.println("<product name=\"" + algoProduct.getName() + "\">");
         if (algoProduct.getReviews().size() > 0 && !algoProduct.getReviews().get(0).getReview().equals("null")) {
             compareThesisLists(algoProduct.getReviews(), humProduct.getReviews(), out);
         }
@@ -102,55 +135,92 @@ public class ThesisExtractionTestDocument {
         int editDist = 3;
 
         for (int k = 0; k < algoReview.size(); k++) {
-            out.println("   <review id=\"" + algoReview.get(k).getReview() + "\">");
+            String reviewID = algoReview.get(k).getReview();
 
-            ArrayList<String> algoThesis = algoReview.get(k).getThesis();
-            ArrayList<String> humThesis = humReview.get(k).getThesis();
+            if(!reviewID.equals("-1")){
+                out.println("   <review id=\"" + reviewID + "\">");
 
-            numAlgo += algoThesis.size();
-            numHum += humThesis.size();
+                ArrayList<String> algoThesis = algoReview.get(k).getThesis();
+                ArrayList<String> humThesis = humReview.get(k).getThesis();
 
-            for (int i = 0; i < algoThesis.size(); i++) {
-                String s1 = algoThesis.get(i).trim();
-                for (int j = 0; j < humThesis.size(); j++) {
-                    String s2 = humThesis.get(j).trim();
-                    if (EditDistance.editDist(s1, s2) < editDist) {
-                        out.println("      <OK>" + s1 + "</OK>");
-                        successExtract++;
+                numAlgo += algoThesis.size();
+                numHum += humThesis.size();
+
+                for (int i = 0; i < algoThesis.size(); i++) {
+                    String s1 = algoThesis.get(i).trim();
+                    for (int j = 0; j < humThesis.size(); j++) {
+                        String s2 = humThesis.get(j).trim();
+                        if (editDist(s1, s2) < editDist) {
+                            out.println("      <OK>" + s1 + "</OK>");
+                            successExtract++;
+                            break;
+                        }
                     }
                 }
-            }
 
-            for (int i = 0; i < algoThesis.size(); i++) {
-                boolean t = false;
-                String s1 = algoThesis.get(i).trim();
-                for (int j = 0; j < humThesis.size(); j++) {
-                    String s2 = humThesis.get(j).trim();
-                    if (EditDistance.editDist(s1, s2) < editDist) {
-                        t = true;
+                for (int i = 0; i < algoThesis.size(); i++) {
+                    boolean t = false;
+                    String s1 = algoThesis.get(i).trim();
+                    for (int j = 0; j < humThesis.size(); j++) {
+                        String s2 = humThesis.get(j).trim();
+                        if (editDist(s1, s2) < editDist) {
+                            t = true;
+                        }
+                    }
+                    if (t == false) {
+                        out.println("      <algo>" + s1 + "</algo>");
                     }
                 }
-                if (t == false) {
-                    out.println("      <algo>" + s1 + "</algo>");
-                }
-            }
 
-            for (int i = 0; i < humThesis.size(); i++) {
-                boolean t = false;
-                String s1 = humThesis.get(i).trim();
-                for (int j = 0; j < algoThesis.size(); j++) {
-                    String s2 = algoThesis.get(j).trim();
-                    if (EditDistance.editDist(s1, s2) < editDist) {
-                        t = true;
+                for (int i = 0; i < humThesis.size(); i++) {
+                    boolean t = false;
+                    String s1 = humThesis.get(i).trim();
+                    for (int j = 0; j < algoThesis.size(); j++) {
+                        String s2 = algoThesis.get(j).trim();
+                        if (editDist(s1, s2) < editDist) {
+                            t = true;
+                        }
+                    }
+                    if (t == false) {
+                        out.println("      <hum>" + s1 + "</hum>");
                     }
                 }
-                if (t == false) {
-                    out.println("      <hum>" + s1 + "</hum>");
-                }
+                //out.println("   </review>");
             }
-            //out.println("   </review>");
         }
     }
+
+    public static int editDist(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[] D1;
+        int[] D2 = new int[n + 1];
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+        for (int i = 0; i <= n; i++) {
+            D2[i] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            D1 = D2;
+            D2 = new int[n + 1];
+            for (int j = 0; j <= n; j++) {
+                if (j == 0) {
+                    D2[j] = i;
+                } else {
+                    int cost = (s1.charAt(i - 1) != s2.charAt(j - 1)) ? 1 : 0;
+                    if (D2[j - 1] < D1[j] && D2[j - 1] < D1[j - 1] + cost) {
+                        D2[j] = D2[j - 1] + 1;
+                    } else if (D1[j] < D1[j - 1] + cost) {
+                        D2[j] = D1[j] + 1;
+                    } else {
+                        D2[j] = D1[j - 1] + cost;
+                    }
+                }
+            }
+        }
+        return D2[n];
+    }
+
 
 
     public static void main(String[] args) throws IOException {
