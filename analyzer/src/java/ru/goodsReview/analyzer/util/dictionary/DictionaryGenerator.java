449daef0348be776affdf9f8238245286e185fe3
Artemii <artemij.chugreev@gmail.com>
2012-04-01 20:34:04 +0400

    DictionaryGenerator + create dictionary from xml

--- analyzer/src/java/ru/goodsReview/analyzer/util/dictionary/DictionaryGenerator.java
+++ analyzer/src/java/ru/goodsReview/analyzer/util/dictionary/DictionaryGenerator.java
@@ -9,14 +9,18 @@ package ru.goodsReview.analyzer.util.dictionary;
 */
 
 import org.apache.log4j.Logger;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import ru.goodsReview.analyzer.util.FrequencyDictionary;
 import ru.goodsReview.analyzer.wordAnalyzer.PorterStemmer;
+import ru.goodsReview.core.utils.HashMapUtil;
 
+import javax.xml.xpath.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.TimerTask;
+import java.util.*;
 
 public class DictionaryGenerator extends TimerTask {
     private static Scanner scanner;
@@ -46,6 +50,85 @@ public class DictionaryGenerator extends TimerTask {
             printWriter.println(obj.toString());
         }
         printWriter.close();
+    }
+
+    /**
+     * Checks if letter belongs to russian alphabet.
+     * @param letter The letter itself.
+     * @return True if letter is russian, false — otherwise.
+     */
+    private static boolean isRussianLetter (char letter) {
+        if ((letter >= 0x0410) && (letter <= 0x044F)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean hasWordBadSymbols(String word){
+        char [] wordCharacters = word.toCharArray();
+        for(int i=0; i<wordCharacters.length; i++){
+            if(!isRussianLetter(wordCharacters[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void frequencyDictionaryFromNormalized() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("normalized_words.txt"));
+        HashMap<String, Integer> frequencyDictionary = new HashMap<>();
+        String token = "";
+        while (scanner.hasNext()) {
+            token = scanner.next();
+            if (!token.equals("")) {
+                if(hasWordBadSymbols(token)){
+                    FrequencyDictionary.addWord(frequencyDictionary, token);
+                }
+            }
+        }
+        frequencyDictionary = (HashMap<String, Integer>) HashMapUtil.sortByValue(frequencyDictionary);
+
+        PrintWriter printWriter = new PrintWriter(new File("dictionaryFromNormalized.txt"));
+        for (String str : frequencyDictionary.keySet()) {
+            printWriter.println(str + " " + frequencyDictionary.get(str));
+        }
+        printWriter.close();
+
+    }
+
+    private static void extractWordsFromDataset() throws XPathExpressionException, FileNotFoundException {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        File file = new File("cameras.xml");
+        {
+            XPathExpression xPathExpression = xPath.compile("//value[@columnNumber>'4']/text()");
+            InputSource inputSource = new InputSource(new FileReader(file));
+            Object object = xPathExpression.evaluate(inputSource, XPathConstants.NODESET);
+            NodeList nodeList = (NodeList) object;
+            HashSet<String> hashSet = new HashSet<>();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                StringTokenizer stringTokenizer = new StringTokenizer(nodeList.item(i).getNodeValue(), " .,-—:;!()+\'\"\\«»");
+                while (stringTokenizer.hasMoreTokens()) {
+                    String token = stringTokenizer.nextToken();
+                    hashSet.add(token);
+                }
+            }
+            PrintWriter printWriter = new PrintWriter(new File("source_dictionary.txt"));
+            for(String str : hashSet){
+                printWriter.println(str);
+            }
+
+            printWriter.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        //          frequencyDictionary();
+        try {
+            frequencyDictionaryFromNormalized();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
     @Override
